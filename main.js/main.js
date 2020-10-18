7e97963aefdf526bb7a73c64d9648ad6b78b302a
João Flavo Costa Teixeira Filho <72360863+Joaoflavo@users.noreply.github.com>
2020-10-18 02:24:04 -0300

    quinto

---
+++ main.js/main.js
@@ -0,0 +1,17 @@
+.progressBar
+    padding-top: 30px;
+        position: relative;
+        width: 100%;
+        height: height:10px;
+        backgroud-color: #000;
+        
+
+    .timeBar
+    {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 0;
+        height: 100%;
+        background-color: #ccc; 
+}
