035ffe946d668bbaf3b2711a16aaf7a3fadf517c
João Flavo Costa Teixeira Filho <72360863+Joaoflavo@users.noreply.github.com>
2020-10-18 07:19:14 -0300

    oitavo

--- main.js/main.js
+++ main.js/main.js
@@ -1,22 +1,5 @@
-.autoplay{
-    @media screen {   
-            
-    }
-}
 //return a DOM object
 var video = document.getElementById('videoID'); //or
 var video = $('#videoID').get(0); //or
 var video = $('#videoID')[0];
  
-//return a jQuery object
-var video = $('#videoID');
-//Play/Pause control clicked
-$('.btnPlay').on('click', function() {
-    if(video[0].paused) {
-        video[0].play();
-    }
-    else {
-        video[0].pause();
-    }
-    return false;
-};
